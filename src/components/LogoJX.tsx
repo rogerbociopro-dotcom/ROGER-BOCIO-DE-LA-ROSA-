7eeea0fea6085fac669a72d912a69ed3eb1265e7
rogerbociopro-dotcom <rogerbocio.pro@gmail.com>
2026-05-26 16:29:50 -0400

    Update LogoJX.tsx

--- src/components/LogoJX.tsx
+++ src/components/LogoJX.tsx
@@ -1,7 +1,7 @@
 export default function LogoJX({ className = "h-8 w-8" }: { className?: string }) {
   return (
     <img 
-      src="/src/assets/images/logo-nuevo.png.jpeg" 
+      src="/logo-nuevo.png.jpeg" 
       alt="Juleonix Digital Logo" 
       className={className} 
     />
